4587d41ae8fd9a44978ad0922207e006bb605ac1
Gulnozai Khodizoda <gul.khodizoda@gmail.com>
2020-07-19 01:57:59 -0400

    add struct

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: gkhodizo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/27 14:17:59 by gkhodizo          #+#    #+#             */
-/*   Updated: 2020/07/15 00:41:52 by gkhodizo         ###   ########.fr       */
+/*   Updated: 2020/07/19 01:56:56 by gkhodizo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,32 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdarg.h>
-# include "libft/libft.h"
+# include <stdio.h> 	//remove
 
-int		ft_printf(const char *format, ...);
+# include "./libft/libft.h"
 
-/* move to libft */
-char	*ft_itoa_base(int num, int base);
+typedef struct	s_flags
+{
+	size_t		is_minus;
+	size_t		is_zero;
+	size_t 		width;
+	size_t		precision;
+	char		specifier;
+	size_t		buff_len;
+	char 		*buff;
+}				t_flags;
 
-/* printf specific functions */
+/*
+** printf specific functions
+*/
+
+int				ft_printf(const char *format, ...);
+void			parse_input(char *str, va_list *ap);
+
+/*
+** move to libft
+*/
+
+char			*ft_itoa_base(int num, int base);
 
 #endif
