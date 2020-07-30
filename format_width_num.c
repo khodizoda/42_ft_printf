47127b1b1b242b53d978b45f5869a8d3b1984816
Gulnozai Khodizoda <gul.khodizoda@gmail.com>
2020-07-30 01:25:35 -0400

    fixed format with '0' flag for %u

--- format_width_num.c
+++ format_width_num.c
@@ -6,7 +6,7 @@
 /*   By: gkhodizo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/24 15:15:11 by gkhodizo          #+#    #+#             */
-/*   Updated: 2020/07/30 00:01:06 by gkhodizo         ###   ########.fr       */
+/*   Updated: 2020/07/30 01:23:55 by gkhodizo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ void	format_width_num(t_fmt *fmt)
 {
 	if (fmt->width > fmt->value_len)
 	{
-		if (fmt->is_zero && (!fmt->is_minus) && (!fmt->precision))
+		if (fmt->is_zero && (!fmt->is_minus)
+			&& (!fmt->is_precision || fmt->negative_prec))
 			fmt->spec_value = format_padding(fmt, '0', fmt->width, 0);
 		else
 			fmt->spec_value = format_padding(fmt, ' ',
