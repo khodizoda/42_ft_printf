62a29f1be5f1b2bb2855f1dddd66c8a54d5cc274
Gulnozai Khodizoda <gul.khodizoda@gmail.com>
2020-07-31 00:35:36 -0400

    added '\0' at the end of new str

--- libft/free_n_copy.c
+++ libft/free_n_copy.c
@@ -6,19 +6,15 @@
 /*   By: gkhodizo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/24 15:48:26 by gkhodizo          #+#    #+#             */
-/*   Updated: 2020/07/30 19:32:04 by gkhodizo         ###   ########.fr       */
+/*   Updated: 2020/07/31 00:33:33 by gkhodizo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-** Allocates (with malloc(3)) and returns a “fresh” substring from the string
-** given as argument. The substring begins at index start and is of size len.
-** After copy is done, function calls ft_strdel() to free(3) given str.
-** It is the caller's responsibility to make sure that given str was malloc'ed
-** before it is passed to free_n_copy() and can be free'd.
-** The free_n_copy() DOES NOT null-terminate new string.
-** If start and len aren’t refering to a valid substring, the behavior is
-** undefined.
+** The free_n_copy(), copies str to new string using ft_strsub() and
+** calls ft_strdel() to free(3) given str. It is the caller's responsibility
+** to make sure that given str was malloc'ed before it is passed to
+** free_n_copy() and can be free'd. Otherwise use directly ft_strsub().
 ** Returns new string. If the allocation fails, the function returns NULL.
 */
 
@@ -26,13 +22,9 @@
 
 char	*free_n_copy(char *str, int start, size_t len)
 {
-	size_t	i;
 	char	*new;
 
-	i = 0;
-	if (!(new = (char *)malloc(len * sizeof(char))))
-		return (NULL);
-	ft_strncpy(new, (str + start), len);
+	new = ft_strsub(str, start, len);
 	ft_strdel(&str);
 	return (new);
 }
