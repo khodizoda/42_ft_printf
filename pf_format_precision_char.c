1e2690ed47bbe4125bf47a60c4b9c31f2a9f9b10
Gulnozai Khodizoda <gul.khodizoda@gmail.com>
2020-08-01 18:49:34 -0400

    upd headers

--- pf_format_precision_char.c
+++ pf_format_precision_char.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   format_precision_char.c                            :+:      :+:    :+:   */
+/*   pf_format_precision_char.c                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gkhodizo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/24 15:14:46 by gkhodizo          #+#    #+#             */
-/*   Updated: 2020/07/31 22:46:22 by gkhodizo         ###   ########.fr       */
+/*   Updated: 2020/08/01 18:44:23 by gkhodizo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@
 
 void	format_precision_char(t_fmt *fmt)
 {
-	if (fmt->specifier == 'p' && fmt->is_precision && fmt->is_null)
+	if (fmt->specifier == 'p' && fmt->is_precision
+		&& fmt->precision == 0 && fmt->negative_prec == 0
+		&& fmt->is_null)
 	{
 		fmt->spec_value = free_n_copy(fmt->spec_value, 0, 2);
 		fmt->value_len = 2;
