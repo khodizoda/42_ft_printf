f29274e35708c27fe4131728c7b4f810c16927e2
Gulnozai Khodizoda <gul.khodizoda@gmail.com>
2020-08-02 12:15:03 -0400

    norm fix

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,19 +6,19 @@
 /*   By: gkhodizo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 16:37:28 by gkhodizo          #+#    #+#             */
-/*   Updated: 2020/07/23 23:34:35 by gkhodizo         ###   ########.fr       */
+/*   Updated: 2020/08/02 12:14:23 by gkhodizo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-** Allocates (with malloc(3)) and returns a “fresh” string ending with ’\0’
+** Allocates (with malloc(3)) and returns a “fresh” string ending with '\0'
 ** representing the integer n given as argument. Negative numbers must be
 ** supported. If the allocation fails, the function returns NULL.
 */
 
 #include "libft.h"
 
-char			*ft_itoa(int n)
+char	*ft_itoa(int n)
 {
 	long	num;
 	size_t	len;
@@ -26,8 +26,7 @@ char			*ft_itoa(int n)
 
 	num = n;
 	len = ft_int_len(num);
-	res = (char *)malloc((len + 1) * sizeof(char));
-	if (!res)
+	if (!(res = (char *)malloc((len + 1) * sizeof(char))))
 		return (NULL);
 	if (num < 0)
 		res[0] = '-';
